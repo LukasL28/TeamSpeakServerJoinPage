b6a8c879b8026229b2f465cc18170a3bfa98dcf8
Lukas <57566886+LukasL28@users.noreply.github.com>
2022-12-20 21:06:42 +0100

    ws stuff

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const ws = require("ws");
 const cors = require("cors");
+const { json } = require("express");
+require('dotenv').config();
 
 const app = express();
 const server = require("http").createServer(app);
@@ -8,6 +10,16 @@ const server = require("http").createServer(app);
 const wss = new ws.Server({
     server: server,
 });
+
+var data = [{
+    Server_Name: process.env.SERVER_NAME, 
+    Server_Icon_URL: process.env.SERVER_ICON_URL, 
+    Server_Address: process.env.SERVER_ADDRESS, 
+    Server_Port: process.env.SERVER_PORT,
+    Favicon: process.env.FAVICON
+}]
+
+console.log(data)
 
 server.listen(8080, () => {
     console.log("Webserver läuft und hört auf Port ", 8080); //successful start notification
@@ -19,14 +31,6 @@ wss.on(
     "connection",
     (connection = (ws) => {
         console.log("A new client Connected!");
-        const logEvery200mSeconds = (i400ms) => {
-            //loop for realtime stuff
-            setTimeout(() => {
-                // Tis will be run every 400 ms
-                ws.send("Hello from the server!");
-                logEvery200mSeconds(++i400ms);
-            }, 400);
-        };
-        logEvery200mSeconds(0);
+        ws.send(JSON.stringify(data));
     })
 );
