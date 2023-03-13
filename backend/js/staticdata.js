3c8811e0718a9496b2948881d67dd853e23b113e
Lukas <57566886+LukasL28@users.noreply.github.com>
2023-03-13 22:59:32 +0100

    logic init

--- backend/js/staticdata.js
+++ backend/js/staticdata.js
@@ -3,14 +3,33 @@ require('dotenv').config();
 var data = [{
     Server_Name: process.env.SERVER_NAME, 
     Server_Icon_URL: process.env.SERVER_ICON_URL, 
-    Server_Address: process.env.SERVER_ADDRESS, 
-    Server_Port: process.env.SERVER_PORT,
-    Server_Bookmark: process.env.SERVER_BOOKMARK,
-    Server_Password: process.env.SERVER_PASSWORD,
-    Join_Channel: process.env.JOIN_CHANNEL,
-    Join_Channel_Password: process.env.JOIN_CHANNEL_PASSWORD,
-    Token: process.env.TOKEN,
+    url: create_link(),
     Favicon: process.env.FAVICON
 }]
+
+function create_link() {
+    var link = "ts3server://" + process.env.SERVER_ADDRESS
+    if (process.env.SERVER_PORT != "") {
+        link += "?port=" + process.env.SERVER_PORT
+    } else {
+        link += "?port=9987" //default port, so the other parameters don't break if no port is set
+    }
+    if (process.env.SERVER_BOOKMARK != "") {
+        link += "&addbookmark=" + process.env.SERVER_BOOKMARK
+    }
+    if (process.env.SERVER_PASSWORD != "") {
+        link += "&password=" + process.env.SERVER_PASSWORD
+    }
+    if (process.env.JOIN_CHANNEL != "") {
+        link += "&channel=" + process.env.JOIN_CHANNEL
+    }
+    if (process.env.JOIN_CHANNEL_PASSWORD != "") {
+        link += "&channelpassword=" + process.env.JOIN_CHANNEL_PASSWORD
+    }
+    if (process.env.TOKEN != "") {
+        link += "&token=" + process.env.TOKEN
+    }
+    return link
+}
 
 module.exports = data;
