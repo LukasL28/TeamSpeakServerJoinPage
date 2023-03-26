658ee01ecd03b416d9cfa76d41e08d20e3bbcf48
Lukas <57566886+LukasL28@users.noreply.github.com>
2023-03-26 16:19:13 +0200

    check for null as well

--- backend/js/staticdata.js
+++ backend/js/staticdata.js
@@ -12,24 +12,25 @@ var data = [
 
 function create_link() {
     var link = "ts3server://" + process.env.SERVER_ADDRESS;
-    if (process.env.SERVER_PORT != "") {
+    // check if port is set, if not use default port
+    if (process.env.SERVER_PORT != "" && process.env.SERVER_PORT != null) {
         link += "?port=" + process.env.SERVER_PORT;
     } else {
         link += "?port=9987"; //default port, so the other parameters don't break if no port is set
     }
-    if (process.env.SERVER_BOOKMARK != "") {
+    if (process.env.SERVER_BOOKMARK != "" && process.env.SERVER_BOOKMARK != null) {
         link += "&addbookmark=" + process.env.SERVER_BOOKMARK;
     }
-    if (process.env.SERVER_PASSWORD != "") {
+    if (process.env.SERVER_PASSWORD != "" && process.env.SERVER_PASSWORD != null) {
         link += "&password=" + process.env.SERVER_PASSWORD;
     }
-    if (process.env.JOIN_CHANNEL != "") {
+    if (process.env.JOIN_CHANNEL != "" && process.env.JOIN_CHANNEL != null) {
         link += "&channel=" + process.env.JOIN_CHANNEL;
     }
-    if (process.env.JOIN_CHANNEL_PASSWORD != "") {
+    if (process.env.JOIN_CHANNEL_PASSWORD != "" && process.env.JOIN_CHANNEL_PASSWORD != null) {
         link += "&channelpassword=" + process.env.JOIN_CHANNEL_PASSWORD;
     }
-    if (process.env.TOKEN != "") {
+    if (process.env.TOKEN != "" && process.env.TOKEN != null) {
         link += "&token=" + process.env.TOKEN;
     }
     return link;
